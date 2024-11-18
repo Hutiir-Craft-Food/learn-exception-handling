4219705a509e3a9e1eb86ea748e5459fb5f2d20f
P.F. <pavel.filkovskiy@gmail.com>
2024-11-18 22:07:11 +0000

    use the same type for timestamp as spring in custom error response

--- src/main/java/com/learn/exceptionhandling/exception/BlahErrorResponse.java
+++ src/main/java/com/learn/exceptionhandling/exception/BlahErrorResponse.java
@@ -3,18 +3,18 @@ package com.learn.exceptionhandling.exception;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Builder
 public record BlahErrorResponse(
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd\'T\'HH:mm:ss.SSS")
-        LocalDateTime timestamp,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd\'T\'HH:mm:ss.SSS\'ZZZ")
+        Date timestamp,
         int status,
         String error,
         String message,
         String path
 ) {
     public BlahErrorResponse {
-        timestamp = LocalDateTime.now();
+        timestamp = new Date();
     }
 }
