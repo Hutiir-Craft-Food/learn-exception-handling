648edc786db88f59665fd48f8d9a8b2d4ab1d8e0
P.F. <pavel.filkovskiy@gmail.com>
2025-01-24 21:36:10 +0000

    refactor

--- src/main/java/com/learn/exceptionhandling/exception/AnnotatedBlahException.java
+++ src/main/java/com/learn/exceptionhandling/exception/AnnotatedBlahException.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
 public class AnnotatedBlahException extends RuntimeException {
-
     public AnnotatedBlahException(String message) {
         super(message);
     }
